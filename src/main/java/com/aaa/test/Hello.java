e8f3ff9f42cf33d020abe5f2cb8b82ee49572a31
githubhyz0418 <969714788@qq.com>
2019-04-18 14:17:44 +0800

    修改了hello.44java

--- src/main/java/com/aaa/test/Hello.java
+++ src/main/java/com/aaa/test/Hello.java
@@ -8,6 +8,6 @@ package com.aaa.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello1");
+        System.out.println("hello1jnmj");
     }
 }
